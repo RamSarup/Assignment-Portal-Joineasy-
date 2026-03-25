6d5424353566aef3b3c480a28170be431ff8ceb9
Ramsarup <ramsarup516@gmail.com>
2026-03-26 01:57:40 +0530

    Added the local storage feature so that the data remains on reload

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,29 +1,31 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import StudentDashboard from "./pages/StudentDashboard";
 import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
   const [role, setRole] = useState("student");
 
-  const [assignments, setAssignments] = useState([
-    { id: 1, title: "DBMS Assignment", submitted: false },
-    { id: 2, title: "OS Assignment", submitted: false },
-  ]);
+  const [assignments, setAssignments] = useState(() => {
+    const saved = localStorage.getItem("assignments");
+    return saved
+      ? JSON.parse(saved)
+      : [{ id: 1, title: "DBMS Assignment", submitted: false }];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("assignments", JSON.stringify(assignments));
+  }, [assignments]);
 
   return (
     <div className="min-h-screen bg-blue-100 flex flex-col items-center p-4">
-      
-      <h1 className="text-2xl font-bold mb-4">
-        Assignment Dashboard
-      </h1>
+      <h1 className="text-2xl font-bold mb-4">Assignment Dashboard</h1>
 
       <div className="flex gap-4 mb-6">
         <button
           onClick={() => setRole("student")}
           className={`px-4 py-2 rounded ${
-            role === "student"
-              ? "bg-blue-600 text-white"
-              : "bg-gray-300"
+            role === "student" ? "bg-blue-600 text-white" : "bg-gray-300"
           }`}
         >
           Student
@@ -32,9 +34,7 @@ function App() {
         <button
           onClick={() => setRole("admin")}
           className={`px-4 py-2 rounded ${
-            role === "admin"
-              ? "bg-green-600 text-white"
-              : "bg-gray-300"
+            role === "admin" ? "bg-green-600 text-white" : "bg-gray-300"
           }`}
         >
           Admin
